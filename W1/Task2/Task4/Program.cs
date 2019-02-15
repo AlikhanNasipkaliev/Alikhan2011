7a7727f1fd5afcd518fbd8f3bd905cd7d1974200
AlikhanNasipkaliev <a.nasypkaliev11@mail.ru>
2019-02-15 20:05:01 +0300

    tw3

--- W1/Task2/Task4/Program.cs
+++ W1/Task2/Task4/Program.cs
@@ -6,35 +6,35 @@ using System.Threading.Tasks;
 
 namespace Task_2
 {
-    class Student
+    class Student//классты ашамыз 
     {
-        public string name;
+        public string name;//публичный строка етеміз,классты жапқаннан кейінде оны қолдану үшін
         public string id;
         public int year = 1;
 
-        public void incc()
+        public void incc()//метод,енгізген жылға бір қосып қояды
         {
             year++;
         }
 
-        public Student(string nam, string idd)
+        public Student(string nam, string idd)//конструктор,егер атын,айдиын енгізссек осы конструторға келеді
         {
             name = nam;
             id = idd;
-            incc();
+            incc();//методыты шақырады
             print();
 
         }
-        public Student()
+        public Student()//конструктор егер дым жазылмаса,онда атын,айдиын өзіміз жазамыз 
         {
             name = Console.ReadLine();
             id = Console.ReadLine();
-            year = int.Parse(Console.ReadLine());
+            year = int.Parse(Console.ReadLine());//Parse жазған симболды санға айналдырады
             year++;
             print();
 
         }
-        public void print()
+        public void print()//метод: атын айдиын атын шығарады
         {
             Console.WriteLine("Student's name: " + name);
             Console.WriteLine("Student's id: " + id);
@@ -47,8 +47,8 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Student Ali = new Student("Ali", "18BD111222");
-            Student Maks = new Student();
+            Student Ali = new Student("Ali", "18BD111222");//егер атын және айдиын жазсақ
+            Student Maks = new Student();//егер дым жазбасақ онда конструкторға барады,сол жерде өзіміз жазамыз
         }
     }
 }
